8062ed832e304fb1b18ef22136e7bf4cd2baf578
keynes <fangjun@zdsoft.cn>
2018-10-31 22:53:30 +0800

    up

--- components/play_video.js
+++ components/play_video.js
@@ -3,11 +3,20 @@ var subtitles_file = require("../resource/subtitles.js");
 
 Component({
   VideoContext: "",
+  video_height: 0,
+  subtitles_height: 0,
 
   ready: function () {
-    console.log(wx.getSystemInfoSync().windowHeight);
     this.VideoContext = wx.createVideoContext("current_video", this);
-   },
+    this.video_height = wx.getSystemInfoSync().windowWidth / 16 * 9;
+    console.log(this.video_height);
+    this.subtitles_height = wx.getSystemInfoSync().windowHeight - 70 - 30 - this.video_height;
+    this.setData({
+      video_height: video_height,
+      subtitles_height: subtitles_height
+    });
+  },
+
   properties: {
 
   },
@@ -26,21 +35,23 @@ Component({
    * Component methods
    */
   methods: {
-    switch_view_model: function(e) {
+    switch_view_model: function (e) {
       if (this.data.view_model == "阅读模式") {
         this.data.view_model = "视频模式";
-        this.data.play_status = "pause";
+        var temp_subtitles_height = this.subtitles_height + this.video_height;
       } else {
         this.data.view_model = "阅读模式";
         this.data.play_status = "pause";
+        var temp_subtitles_height = this.subtitles_height;
       }
       this.setData({
         view_model: this.data.view_model,
-        play_status: this.data.play_status
+        play_status: this.data.play_status,
+        subtitles_height: temp_subtitles_height
       });
     },
 
-    video_play_control: function(e) {
+    video_play_control: function (e) {
       if (this.data.play_status == "pause") {
         this.VideoContext.play();
         this.data.play_status = "play";
