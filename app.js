3a236f2d9479e6d7bd48a8426dc3c43a86dd3f83
keynes <fangjun@zdsoft.cn>
2018-11-13 23:12:37 +0800

    up

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
 //app.js
 App({
   onLaunch: function() {
-    // 展示本地存储能力
-    // var logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
-
-    // 登录
-    var that = this;
-    wx.login({
-      success: function(res) {
-        console.log(res)
-        if (res.code) {
-          //存在code
-          wx.request({
-            url: 'https://iperson.top/wx_login.php',
-            data: {
-              req_type: "login",
-              code: res.code
-            },
-            method: 'POST',
-            header: {
-              "content-type": "application/x-www-form-urlencoded"
-            },
-            success: function(res) {
-              that.globalData.userId = res.data.info
-              console.log(res.data);
-            },
-            fail: function() {
-              console.log('服务器请求失败!');
-            },
-          })
-        } else {
-          console.log('获取用户信息失败!' + res.errMsg)
-        }
-      }
-    });
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -49,7 +14,7 @@ App({
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                this.userInfoReadyCallback()
               }
             }
           })
@@ -57,6 +22,54 @@ App({
       }
     })
   },
+  userInfoReadyCallback: function() {
+    // 登录远程服务器
+    var that = this;
+    wx.login({
+      success: function(res) {
+        if (res.code) {
+          //存在code
+          that.globalData.code = res.code;
+          wx.getLocation({
+            type: 'wgs84',
+            success(res) {
+              that.globalData.userInfo.latitude = res.latitude;
+              that.globalData.userInfo.longitude = res.longitude;
+              console.log(that.globalData.userInfo);
+              wx.request({
+                url: 'https://iperson.top/wx_login.php',
+                data: {
+                  req_type: "login",
+                  code: that.globalData.code,
+                  nickName: that.globalData.userInfo.nickName,
+                  avatarUrl: that.globalData.userInfo.avatarUrl,
+                  gender: that.globalData.userInfo.gender,
+                  latitude: res.latitude,
+                  longitude: res.longitude
+                },
+                method: 'POST',
+                header: {
+                  "content-type": "application/x-www-form-urlencoded"
+                },
+                success: function(res) {
+                  that.globalData.openid = res.data.openid;
+                  console.log(that.globalData.openid);
+                },
+                fail: function() {
+                  that.globalData.openid = "_error";
+                  console.log('服务器请求失败!');
+                },
+              })
+            }
+          });
+        } else {
+          that.globalData.openid = "_error";
+          console.log('获取用户信息失败!' + res.errMsg)
+        }
+      }
+    });
+  },
+
   globalData: {
     userInfo: null
   }
