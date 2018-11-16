c9401d56cc737a960a982ee06b0e6f12602dc621
zds-fangjun <fangjun@zdsoft.cn>
2018-11-16 16:43:02 +0800

    up

--- resource/video_list.js
+++ resource/video_list.js
@@ -8,7 +8,8 @@ var videos = {
   "教育理念": [
     { "index": "0", "title": "每个孩子都是冠军", "name": "Every kid needs a champion", "time": "07:44", "file": "Every_kid_needs_a_champion" },
     { "index": "1", "title": "激发学习的3条规则", "name": "3 rules to spark learning", "time": "06:29", "file": "3_rules_to_spark_learning" },
-    { "index": "2", "title": "从黑帮女儿到明星老师", "name": "From gangland daughter to star teacher", "time": "07:58", "file": "From_gangland_daughter_to_star_teacher" }
+    { "index": "2", "title": "从黑帮女儿到明星老师", "name": "From gangland daughter to star teacher", "time": "07:58", "file": "From_gangland_daughter_to_star_teacher" },
+    { "index": "3", "title": "教师需要真实的反馈", "name": "Teachers need real feedback", "time": "10:18", "file": "Teachers_need_real_feedback" }
   ]
 };
 
