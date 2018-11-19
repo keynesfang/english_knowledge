e9dc1cdd2420dba6f0b882d445ef9da7da03f6e3
keynes <fangjun@zdsoft.cn>
2018-11-19 23:02:12 +0800

    up

--- resource/video_list.js
+++ resource/video_list.js
@@ -1,6 +1,6 @@
 var videos = {
   "时间管理": [
-    { "index": "0", "title": "高效管理碎片时间", "name": "Yow to gain control of your free time", "time": "11:54", "file": "How_to_gain_control_of_your_free_time" }
+    { "index": "0", "title": "高效管理碎片时间", "name": "How to gain control of your free time", "time": "11:54", "file": "How_to_gain_control_of_your_free_time" }
   ],
   "高端知识": [
     { "index": "0", "title": "指纹泄露了你的信息", "name": "Your fingerprints reveal more than you think", "time": "10:06", "file": "Your_fingerprints_reveal_more_than_you_think" }
